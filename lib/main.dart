a4aeff27648ec60532b9af4d81999e7fa6124cae
Dennis Bozzi <dennisbozzii@gmail.com>
2023-07-28 20:24:17 -0300

    Atualizando

--- lib/main.dart
+++ lib/main.dart
@@ -22,25 +22,19 @@ class MyApp extends StatelessWidget {
     );
   }
 }
-
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
-
   final String title;
-
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
-
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
-
   void _incrementCounter() {
     setState(() {
       _counter++;
     });
   }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
